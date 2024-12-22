eb3a0d4514b548aae8c2060f19a5ffa0b9c462d9
agnoam <agnoam.aghai.ooo.123@gmail.com>
2024-12-22 03:40:22 +0200

    Added test file

---
+++ tests/third/test.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <assert.h>
+
+#include "../dist/third_assignment.c"
+
+void test_q_two() {
+    assert(number_of_bin_ones(0) == 0);
+    assert(number_of_bin_ones(1) == 1);
+    assert(number_of_bin_ones(5) == 2);
+    assert(number_of_bin_ones(6) == 2);
+    assert(number_of_bin_ones(10) == 2);
+    assert(number_of_bin_ones(13) == 3);
+}
+
+void test_q_three() {
+    assert(diff(1234567) == -4);
+    assert(diff(521376) == -8);
+    assert(diff(12) == 1);
+    assert(diff(19) == -10);
+}
+
+int main() {
+    printf("Starting to run tests...\n");
+
+    test_q_two();
+    printf("q_two tests passed.\n");
+    test_q_three();
+    printf("q_three tests passed.\n");
+
+    return 0;
+}
+
